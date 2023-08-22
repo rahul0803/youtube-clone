b00f85953315e49cc3336854dbbc58371a64f339
Rahul <royrahulrk12@gmail.com>
2023-08-22 14:49:32 +0530

    youtube-clone

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/youtube-clone" exact element={<Feed />} /> 
+        <Route path="/" exact element={<Feed />} /> 
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
